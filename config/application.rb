5038bd29907bd7aab9d56b7cd8b6e16dcc0aadc7
takeru.hashimoto <hstake25@gmail.com>
2022-10-22 22:19:19 +0900

    domain/* 読み込む

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("domain")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
